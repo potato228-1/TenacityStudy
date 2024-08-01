16a06b114b2d0d9209e62eb73d1eb8ad4a840acf
R <petruhakabachok@gmail.com>
2024-08-01 12:56:54 +0800

    Updates

--- src/components/SomeFooter.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <footer class="w-full p-10 text-center flex flex-col items-center justify-center bg-black text-[#009393]">
-        <h1 class=" font-black text-xl">FOOTER</h1>
-    </footer>
-</template>
--- src/components/UI/index.js
+++
@@ -1,7 +0,0 @@
-import SomeBtn from '@/components/UI/SomeBtn.vue'
-import SomeInput from '@/components/UI/SomeInput.vue'
-
-export default [
-    SomeBtn,
-    SomeInput,
-]
