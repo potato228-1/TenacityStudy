0b75463863d0c3faf2d86ce30acc55a1c34f7524
R <petruhakabachok@gmail.com>
2024-08-01 12:55:15 +0800

    some-comit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+    publicPath: `/TenacityStudy/`,
 	transpileDependencies: true,
-    publicPath: '/TenacityStudy/'
 });
